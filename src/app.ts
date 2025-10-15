59db4ddc8b9566727e5d1df0e88472fe69246780
kodepisang <bananacode7@gmail.com>
2025-10-15 16:56:22 +0700

    add CORS middleware with custom options for API

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,30 @@ import agamaRoutes from "./routes/AgamaRoutes.js";
 import jabatanRoutes from "./routes/JabatanRoutes.js";
 import KlasifikasiDesaRoutes from "./routes/KlasifikasiDesaRoutes.js";
 import PendidikanRoutes from "./routes/PendidikanRoutes.js";
-// import router dari routes/index.ts
+import cors, { type CorsOptions } from 'cors';
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || []
+// CORS
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        // Izinkan request tanpa origin (misalnya: Postman, permintaan dari file:///, atau yang sama)
+        if (!origin) return callback(null, true);
 
+        // Cek apakah origin yang meminta ada di daftar yang diizinkan
+        if (allowedOrigins.indexOf(origin) !== -1 || allowedOrigins.length === 0) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'), false);
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Metode HTTP yang diizinkan
+    credentials: true, // Izinkan cookies dikirim
+    optionsSuccessStatus: 204 // Standar untuk pre-flight request
+};
 // Middleware
+app.use(cors(corsOptions)); // milddleware CORS opsi custom
 app.use(express.json()); // Body parser untuk JSON
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
@@ -25,10 +43,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('SAPADESA Backend is running! (Sistem Informasi Aparatur Desa)');
 });
 
-// Panggil Route Utama di sini
-// app.use('/api/v1', mainRouter);
-
-// Server Start
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
